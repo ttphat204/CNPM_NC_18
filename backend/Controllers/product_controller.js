41ebf6826ed0d9d149ac7b8364ef367e4ad05145
thiendao <thiendaocong@gmail.com>
2024-10-18 08:36:46 +0700

    1

--- backend/Controllers/product_controller.js
+++ backend/Controllers/product_controller.js
@@ -28,8 +28,25 @@ module.exports = {
     }
   },
   getProducts: async (req, res) => {
-    const products = await productModel.find();
+    const category_id = req.query.category_id;
+    const body_query = {};
+    if (category_id) {
+      body_query.category_id = category_id;
+    }
+    const products = await productModel.find(body_query).populate("category");
     return res.status(200).json(products);
   },
+  updateProduct: async (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    const updatedProduct = await productModel.findByIdAndUpdate(id, body, {
+      new: true,
+    });
+    return res.status(200).json(updatedProduct);
+  },
+  deleteProduct: async (req, res) => {
+    const id = req.params.id;
+    const deleteProduct = await productModel.findByIdAndDelete(id);
+    return res.status(200).json(deleteProduct);
+  },
 };
-thien;

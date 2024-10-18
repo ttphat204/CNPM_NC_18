00fa0496a9b1d57f0c33fcea7ab2050052007f9f
thiendao <thiendaocong@gmail.com>
2024-10-18 14:57:51 +0700

    18/10/2024

--- backend/Controllers/NCC_controller.js
+++ backend/Controllers/NCC_controller.js
@@ -10,4 +10,17 @@ module.exports = {
     const NCCs = await NCCModel.find();
     return res.status(200).json(NCCs);
   },
+  updateNCC: async (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    const updatedNCC = await NCCModel.findByIdAndUpdate(id, body, {
+      new: true,
+    });
+    return res.status(200).json(updatedNCC);
+  },
+  deleteNCC: async (req, res) => {
+    const id = req.params.id;
+    const deleteNCC = await NCCModel.findByIdAndDelete(id);
+    return res.status(200).json(deleteNCC);
+  },
 };
--- backend/routers/NCC_router.js
+++ backend/routers/NCC_router.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { createNCC, getNCCs } = require("../Controllers/NCC_controller");
+const {
+  createNCC,
+  getNCCs,
+  updateNCC,
+  deleteNCC,
+} = require("../Controllers/NCC_controller");
 
 router.route("/").post(createNCC).get(getNCCs);
+router.route("/:id").patch(updateNCC).delete(deleteNCC);
 
 module.exports = router;

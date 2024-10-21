0c179a683fd3871185eaf214b0c823cdca2b5397
thiendao <thiendaocong@gmail.com>
2024-10-21 13:40:07 +0700

    dthien

---
+++ backend/models/order_model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const orderSchema = mongoose.Schema({
+  customer: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  total_money: {
+    type: Number,
+    required: true,
+  },
+  payment_method: {
+    type: String,
+    enum: ["COD", "Momo", "Cart Credit"],
+    default: "COD",
+  },
+  is_payment: {
+    type: Boolean,
+    default: false,
+  },
+  cart_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "cart",
+  },
+});
+
+module.exports = mongoose.model("order", orderSchema);
--- backend/routers/index.js
+++ backend/routers/index.js
@@ -4,6 +4,7 @@ const accountRouter = require("./account_router");
 const productRouter = require("./product_router");
 const discountRouter = require("./discount_router");
 const cartRouter = require("./cart_router");
+const orderRouter = require("./order_router");
 
 module.exports = (app) => {
   app.use("/api/categories", categoryRouter);
@@ -12,4 +13,5 @@ module.exports = (app) => {
   app.use("/api/products", productRouter);
   app.use("/api/discounts", discountRouter);
   app.use("/api/carts", cartRouter);
+  app.use("/api/orders", orderRouter);
 };

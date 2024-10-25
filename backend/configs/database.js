5e41d65e5ad365ff974b88c455e81d51126301b3
vothanhtung96 <vothanhtung056@gmail.com>
2024-10-25 16:53:01 +0700

    done

--- backend/configs/database.js
+++ backend/configs/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
     try {
-        await mongoose.connect("mongodb://localhost:27017/Sieuthi")
+        await mongoose.connect("mongodb://localhost:27017/login")
         console.log("kết nối thành công...")
     } catch (error) {
         console.log("thất bại...")
